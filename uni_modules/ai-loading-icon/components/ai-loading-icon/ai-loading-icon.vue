033355f63e1877dfc48b315ff338d5a377c0f9e9
@my_dear_li_pan <526551383@qq.com>
2023-04-13 18:27:51 +0800

    aiview

---
+++ uni_modules/ai-loading-icon/components/ai-loading-icon/ai-loading-icon.vue
@@ -0,0 +1,36 @@
+<!-- 加载图标 -->
+<template>
+	<view class=""> {{$ai.demo}}</view>
+</template>
+<script>
+	// import { demo } from '@/uni_modules/ai-view-ui/index.js';
+	console.log(uni.$ai)
+	export default {
+		data(){
+			return {
+				show: false
+			}
+		},
+		computed:{
+			$ai() {
+			    // #ifndef APP-NVUE
+			    // 在非nvue端，移除props，http，mixin等对象，避免在小程序setData时数据过大影响性能
+			    // return uni.$u.deepMerge(uni.$u, {
+			    //     props: undefined,
+			    //     http: undefined,
+			    //     mixin: undefined
+			    // })
+					return uni.$ai
+			    // #endif
+			    // #ifdef APP-NVUE
+			    return uni.$ai
+			    // #endif
+			},
+		},
+		mounted() {
+			
+		}
+	}
+</script>
+<style>
+</style>
---
+++ uni_modules/ai-loading-icon/readme.md
@@ -0,0 +1,1 @@
+# ai-loading-icon
