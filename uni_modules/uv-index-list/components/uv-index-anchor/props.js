f78dde91d3ab6715a09a389bc53f4bd4f9cb20a9
@my_dear_li_pan <526551383@qq.com>
2023-04-28 10:35:51 +0800

    列表索引

---
+++ uni_modules/uv-index-list/components/uv-index-anchor/props.js
@@ -0,0 +1,30 @@
+export default {
+	props: {
+		// 列表锚点文本内容
+		text: {
+			type: [String, Number],
+			default: ''
+		},
+		// 列表锚点文字颜色
+		color: {
+			type: String,
+			default: '#606266'
+		},
+		// 列表锚点文字大小，单位默认px
+		size: {
+			type: [String, Number],
+			default: 14
+		},
+		// 列表锚点背景颜色
+		bgColor: {
+			type: String,
+			default: '#dedede'
+		},
+		// 列表锚点高度，单位默认px
+		height: {
+			type: [String, Number],
+			default: 32
+		},
+		...uni.$uv.props?.indexAnchor
+	}
+}
---
+++ uni_modules/uv-index-list/components/uv-index-anchor/uv-index-anchor.vue
@@ -0,0 +1,93 @@
+<template>
+	<!-- #ifdef APP-NVUE -->
+	<header>
+	<!-- #endif -->
+	<view
+	    class="uv-index-anchor uv-border-bottom"
+		:ref="`uv-index-anchor-${text}`"
+	    :style="{
+			height: $uv.addUnit(height),
+			backgroundColor: bgColor
+		}"
+	>
+		<text
+		    class="uv-index-anchor__text"
+		    :style="{
+				fontSize: $uv.addUnit(size),
+				color: color
+			}"
+		>{{ text }}</text>
+	</view>
+	<!-- #ifdef APP-NVUE -->
+	</header>
+	<!-- #endif -->
+</template>
+
+<script>
+	import mpMixin from '@/uni_modules/uv-ui-tools/libs/mixin/mpMixin.js'
+	import mixin from '@/uni_modules/uv-ui-tools/libs/mixin/mixin.js'
+	import props from './props.js';
+	// #ifdef APP-NVUE
+	const dom = uni.requireNativePlugin('dom')
+	// #endif
+	/**
+	 * IndexAnchor 列表锚点
+	 * @description 
+	 * @tutorial https://uviewui.com/components/indexList.html
+	 * @property {String | Number}	text	列表锚点文本内容
+	 * @property {String}			color	列表锚点文字颜色 ( 默认 '#606266' )
+	 * @property {String | Number}	size	列表锚点文字大小，单位默认px ( 默认 14 )
+	 * @property {String}			bgColor	列表锚点背景颜色 ( 默认 '#dedede' )
+	 * @property {String | Number}	height	列表锚点高度，单位默认px ( 默认 32 )
+	 * @example <uv-index-anchor :text="indexList[index]"></uv-index-anchor>
+	 */
+	export default {
+		name: 'uv-index-anchor',
+		mixins: [mpMixin, mixin, props],
+		data() {
+			return {
+			}
+		},
+		mounted() {
+			this.init()
+		},
+		methods: {
+			init() {
+				// 此处会活动父组件实例，并赋值给实例的parent属性
+				const indexList = uni.$uv.$parent.call(this, 'uv-index-list')
+				if (!indexList) { 
+					return uni.$uv.error('uv-index-anchor必须要搭配uv-index-list组件使用')
+				}
+				// 将当前实例放入到uv-index-list中
+				indexList.anchors.push(this)
+				const indexListItem = uni.$uv.$parent.call(this, 'uv-index-item')
+				// #ifndef APP-NVUE
+				// 只有在非nvue下，uv-index-anchor才是嵌套在uv-index-item中的
+				if (!indexListItem) {
+					return uni.$uv.error('uv-index-anchor必须要搭配uv-index-item组件使用')
+				}
+				// 设置uv-index-item的id为anchor的text标识符，因为非nvue下滚动列表需要依赖scroll-view滚动到元素的特性
+				indexListItem.id = this.text.charCodeAt(0)
+				// #endif
+			}
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	@import '@/uni_modules/uv-ui-tools/libs/css/components.scss';
+
+	.uv-index-anchor {
+		position: sticky;
+		top: 0;
+		@include flex;
+		align-items: center;
+		padding-left: 15px;
+		z-index: 1;
+
+		&__text {
+			@include flex;
+			align-items: center;
+		}
+	}
+</style>
