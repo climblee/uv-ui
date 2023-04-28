52793ee9d9af81e0b7fde3493b9ec8124a74df36
@my_dear_li_pan <526551383@qq.com>
2023-04-28 10:14:17 +0800

    轮播图结束

---
+++ uni_modules/uv-swiper/components/uv-swiper-indicator/props.js
@@ -0,0 +1,30 @@
+export default {
+	props: {
+		// 轮播的长度
+		length: {
+			type: [String, Number],
+			default: 0
+		},
+		// 当前处于活动状态的轮播的索引
+		current: {
+			type: [String, Number],
+			default: 0
+		},
+		// 指示器非激活颜色
+		indicatorActiveColor: {
+			type: String,
+			default: ''
+		},
+		// 指示器的激活颜色
+		indicatorInactiveColor: {
+			type: String,
+			default: ''
+		},
+		// 指示器模式，line-线型，dot-点型
+		indicatorMode: {
+			type: String,
+			default: ''
+		},
+		...uni.$uv.props?.swiperIndicator
+	}
+}
