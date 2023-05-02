a21a95801168f37da989f6f99be24b68cd9242c6
李攀 <526551383@qq.com>
2023-05-02 13:58:21 +0800

    宫格布局

---
+++ uni_modules/uv-grid/components/uv-grid-item/props.js
@@ -0,0 +1,15 @@
+export default {
+	props: {
+		// 宫格的name
+		name: {
+			type: [String, Number, null],
+			default: null
+		},
+		// 背景颜色
+		bgColor: {
+			type: String,
+			default: 'transparent'
+		},
+		...uni.$uv.props?.gridItem
+	}
+}
---
+++ uni_modules/uv-grid/components/uv-grid/props.js
@@ -0,0 +1,20 @@
+export default {
+	props: {
+		// 分成几列
+		col: {
+			type: [String, Number],
+			default: 3
+		},
+		// 是否显示边框
+		border: {
+			type: Boolean,
+			default: false
+		},
+		// 宫格对齐方式，表现为数量少的时候，靠左，居中，还是靠右
+		align: {
+			type: String,
+			default: 'left'
+		},
+		...uni.$uv.props?.grid
+	}
+}
