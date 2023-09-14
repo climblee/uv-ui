ffcee1bd1cea7c20dbb0899b69ac2703f7d64d9e
@my_dear_li_pan <526551383@qq.com>
2023-09-14 18:11:59 +0800

    dayjs修改

--- uni_modules/uv-ui-tools/libs/util/dayjs.js
+++ uni_modules/uv-ui-tools/libs/util/dayjs.js
@@ -3,9 +3,8 @@ var __commonJS = (cb, mod) => function __require() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
 
-// C:/Users/LP/Downloads/uvui-plus_3.1.27_example/node_modules/dayjs/dayjs.min.js
 var require_dayjs_min = __commonJS({
-  "C:/Users/LP/Downloads/uvui-plus_3.1.27_example/node_modules/dayjs/dayjs.min.js"(exports, module) {
+  "uvuidayjs"(exports, module) {
     !function(t, e) {
       "object" == typeof exports && "undefined" != typeof module ? module.exports = e() : "function" == typeof define && define.amd ? define(e) : (t = "undefined" != typeof globalThis ? globalThis : t || self).dayjs = e();
     }(exports, function() {
@@ -215,4 +214,3 @@ var require_dayjs_min = __commonJS({
   }
 });
 export default require_dayjs_min();
-//# sourceMappingURL=dayjs.js.map
